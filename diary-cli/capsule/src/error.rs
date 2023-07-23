2cd330c2560905ad641de532a4f259dad413d6c3
GreenChild04 <106739622+GreenChild04@users.noreply.github.com>
2023-07-23 12:35:25 +1000

    updated errors to have context

--- diary-cli/capsule/src/error.rs
+++ diary-cli/capsule/src/error.rs
@@ -1,16 +1,23 @@
 use std::{fmt, error::Error};
 pub use crate::gen_error_handler;
-pub use crate::handle_cap_err;
+pub use crate::handle;
+
+#[derive(Debug)]
+pub enum CapErrContext<'a> {
+    WhileZippingFile(&'a str),
+    WhileBuildingTarBall(&'a str),
+}
 
 #[derive(Debug)]
 pub enum CapError {
-    // todo!()
+    IOError(std::io::Error),
 }
 
 impl fmt::Display for CapError {
-    fn fmt(&self, _f: &mut fmt::Formatter) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use CapError::*;
         match self {
-            _ => todo!()
+            IOError(e) => write!(f, "IO Error: {:?}", e),
         }
     }
 }
@@ -22,17 +29,18 @@ pub trait CapErrHandler {
     fn init() -> Self;
 
     /// Run at runtime when an error occurs rather than returning the error (for performance)
-    fn runtime<T>(&self, error: CapError, retry: Option<&dyn Fn() -> Result<T, CapError>>) -> T;
+    fn runtime<T>(&self, error: CapError, context: CapErrContext, retry: Option<&dyn Fn() -> Result<T, CapError>>) -> T;
 }
 
 #[macro_export]
 macro_rules! gen_error_handler {
     ($($pattern:pat => $result:expr),* $(,)?) => {{
         pub struct CapErrHandler;
+        impl Copy for CapErrHandler {}
         impl $crate::capsule::error::CapErrHandler for CapErrHandler {
             fn init() -> Self { Self }
-            fn runtime<T>(&self, error: $crate::capsule::error::CapError, retry: Option<&dyn Fn() -> Result<T, $crate::capsule::error::CapError>>) -> T {
-                match (error, retry) {
+            fn runtime<T>(&self, error: capsule::error::CapError, context: CapErrContext, retry: Option<&dyn Fn() -> Result<T, capsule::error::CapError>>) -> T {
+                match (error, context, retry) {
                     $($pattern => $result),*
                 }
             }
@@ -43,20 +51,41 @@ macro_rules! gen_error_handler {
 }
 
 #[macro_export]
-macro_rules! handle_cap_err {
-    // For use on results with other errors
-    (($handler:ident) $action:expr => $result:expr) => {{
-        let res = $action;
+macro_rules! handle {
+    // For use on results with other errors (once)
+    (($handler:ident $context:expr) ($action:expr) => $result:expr) => {{
+        let res: Result<_, _> = $action;
         if let Err(e) = res {
-            $handler.runtime($result(e), Some(&|| $action))
+            $handler.runtime($result(e), $context, None)
+        } else { res.unwrap() }
+    }};
+
+    // For use on results with other errors
+    (($handler:ident $context:expr) $action:expr => $result:expr) => {{
+        let res: Result<_, _> = $action;
+        if let Err(e) = res {
+            $handler.runtime($result(e), $context, Some(&|| {
+                let res = $action;
+                if let Err(e) = res {
+                    Err($result(e))
+                } else { Ok(res.unwrap()) }
+            }))
+        } else { res.unwrap() }
+    }};
+
+    // For use on results with cap errors (once)
+    (($handler:ident $context:expr) ($action:expr)) => {{
+        let res: Result<_, capsule::error::CapError> = $action;
+        if let Err(e) = res {
+            $handler.runtime(e, $context, None)
         } else { res.unwrap() }
     }};
 
     // For use on results with cap errors
-    (($handler:ident) $action:expr) => {{
-        let res = $action;
+    (($handler:ident $context:expr) $action:expr) => {{
+        let res: Result<_, capsule::error::CapError> = $action;
         if let Err(e) = res {
-            $handler.runtime(e, Some(&|| $action))
+            $handler.runtime(e, $context, Some(&|| $action))
         } else { res.unwrap() }
     }};
 }
--- diary-cli/capsule/src/prelude.rs
+++ diary-cli/capsule/src/prelude.rs
@@ -1,1 +1,1 @@
-pub use super::error::*;
+pub use crate::error::*;
